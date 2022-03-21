68531f3e7dbf067b93667d410c4e34a8ee98e831
Haluk AYDIN <haydin938@gmail.com>
2022-03-21 19:19:59 +0300

    se373homeworkassignment3

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
 
         primarySwatch: Colors.purple,
       ),
-      home: const MyHomePage(title: 'se373homework'),
+      home: const MyHomePage(title: 'se373homework1'),
     );
   }
 }
